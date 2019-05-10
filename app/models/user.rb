955ab48057a2ba7fb44c43f850f14fc455992f4e
daashandayo <dj30c.1@gmail.com>
2019-05-10 22:01:36 +0530

    Add new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :email, presence: true
     validates :password, presence: true
-    validates :password_confirmation, presence: true
 
-    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_144312) do
+ActiveRecord::Schema.define(version: 2019_05_10_155047) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
-    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
